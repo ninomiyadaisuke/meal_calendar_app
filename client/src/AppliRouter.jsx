d4dc3dbce51780e5446bba9ccf7a74a597975827
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-01-19 14:24:53 +0900

    Created page transition function

--- client/src/AppliRouter.jsx
+++ client/src/AppliRouter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { Calendar, UserList, BuyList } from "./template"
+import { Header } from "./components/Header"
 
 const AppliRouter = () => {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path={"(/)?"} component={Calendar} /> 
         <Route exact path={"(/user)"} component={UserList} />
